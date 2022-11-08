359c6378ea3a1f018dce4013b3beafe63ac0c505
iskandari <Alexander.Tedeschi@rystadenergy.com>
2022-11-08 23:31:43 +0100

    refactored streamlit layout and added example

--- project/app.py
+++ project/app.py
@@ -1,3 +1,4 @@
+from os import ST_WRITE
 from neo4j import __version__ as neo4j_version
 import streamlit as st
 from streamlit_folium import st_folium
@@ -54,44 +55,34 @@ class Neo4jConnection:
         return response
 
 
-def hexagons_dataframe_to_geojson(df_hex, file_output=None, column_name="value"):
+def hexagons_dataframe_to_geojson(hex_list: list, file_output = None, column_name = "value"):
     """
     Produce the GeoJSON for a dataframe, constructing the geometry from the "hex_id" column
     and with a property matching the one in column_name
-    """
-    foo = LineString([(-90, 179), (90, 179)])
+    """    
+    foo=  LineString([(-90, 179), (90, 179)])
     bar = LineString([(-90, -179), (90, -179)])
 
     list_features = []
-
-    for i, row in df_hex.iterrows():
-
-        h3.h3_to_geo_boundary(h=row["hex_id"])
-
+    
+    for hex_id in hex_list:
+        
+        h3.h3_to_geo_boundary(hex_id)
+        
         try:
-            geometry_for_row = {
-                "type": "Polygon",
-                "coordinates": [h3.h3_to_geo_boundary(h=row["hex_id"], geo_json=True)],
-            }
-            poly = Polygon(h3.h3_to_geo_boundary(row["hex_id"]))
+            geometry_for_row = { "type" : "Polygon", "coordinates": [h3.h3_to_geo_boundary(hex_id,geo_json=True)]}
+            poly = Polygon(h3.h3_to_geo_boundary(hex_id))
             if not foo.intersects(poly) and not bar.intersects(poly):
-                feature = Feature(
-                    geometry=geometry_for_row,
-                    id=row["hex_id"],
-                    properties={column_name: row[column_name]},
-                )
+                feature = Feature(geometry = geometry_for_row , id=hex_id, properties = {column_name : hex_id})
                 list_features.append(feature)
             else:
-                print("intersected prime meridian!")
+                print('intersected prime meridian!')
         except:
-            print("An exception occurred for hex " + row["hex_id"])
+            print("An exception occurred for hex " + hex_id) 
 
     feat_collection = FeatureCollection(list_features)
     geojson_result = json.dumps(feat_collection)
     return geojson_result
-
-def get_pos(lat,lng):
-    return lat,lng
 
 
 uri = "neo4j://neo4j:7687"
@@ -145,41 +136,85 @@ def shortest_path(from_hex, to_hex):
         """
         res = sess.run(query_string)
         res = [record for record in res]
-        return res 
+        return res[0][0] 
 
+example = pd.read_csv('data/example_path.csv')
+example_dict = example.to_dict(orient='records')
+
+missing_hexes = []
+valid_path = []
+
+for pos in example_dict:
+    if 'prev' not in locals(): 
+        prev = h3.geo_to_h3(pos['latitude'], pos['longitude'], 3)
+        valid_path.append(prev)
+    else:
+        
+        if not h3.h3_indexes_are_neighbors(prev, h3.geo_to_h3(pos['latitude'], pos['longitude'], 3)) \
+            and not (prev == h3.geo_to_h3(pos['latitude'], pos['longitude'], 3)):
+             
+            path = shortest_path(prev, h3.geo_to_h3(pos['latitude'], pos['longitude'], 3))
+            missing_hexes.extend(path)
+        
+        if not (prev == h3.geo_to_h3(pos['latitude'], pos['longitude'], 3)) \
+            and h3.h3_indexes_are_neighbors(prev, h3.geo_to_h3(pos['latitude'], pos['longitude'], 3)):
+            
+                valid_path.append(h3.geo_to_h3(pos['latitude'], pos['longitude'], 3))
+        
+        prev = h3.geo_to_h3(pos['latitude'], pos['longitude'], 3)
+
+
+tab1, tab2 = st.tabs(["gaps", "shortest_path"])
+
+with tab1:
+    missing_path = hexagons_dataframe_to_geojson(missing_hexes, file_output=None, column_name="hex_id")
+    valid_path = hexagons_dataframe_to_geojson(valid_path, file_output=None, column_name="hex_id")
+
+    m = folium.Map(location=[15.70, 114.94], zoom_start=4, tiles="CartoDB positron")
+    geo_j = folium.GeoJson(data=missing_path, style_function=lambda x: {"fillColor": "orange", "fillOpacity": 1})
+    geo_j.add_to(m)
+    geo_j = folium.GeoJson(data=valid_path, style_function=lambda x: {"fillColor": "blue", "fillOpacity": 1})
+    geo_j.add_to(m)
+
+    st.write('The shortest path algorithm can be used to fill in gaps in AIS trajectories. Positional data can be inconsistent in certain geographic regions. Below is an example vessel path with gaps filled in.')
+    example_data = folium_static(m)
 
 
 #mylist = shortest_path("833849fffffffff", "83318dfffffffff")
-with st.sidebar:
+with tab2:
+
+    st.write('Choose two maritime hexagons at resolution 3 to find their shortest maritime path')
+   
+
     form = st.form(key='my-form')
     from_hex = form.text_input('Enter origin hex id')
     to_hex = form.text_input('Enter destination hex id')
     submit = form.form_submit_button('Submit')
 
-if submit:
+    if submit:
 
-    try: 
-        mylist = shortest_path(f'{from_hex}', f'{to_hex}')
+        try: 
+            mylist = shortest_path(f'{from_hex}', f'{to_hex}')
 
-        # #df = pd.DataFrame(res)
-        # # df.to_csv('shortest_path_dijkstra.csv', index=False)
+            # #df = pd.DataFrame(res)
+            # # df.to_csv('shortest_path_dijkstra.csv', index=False)
 
-        res = pd.DataFrame(list(mylist[0][0]), columns=["hex_id"])
+            res = pd.DataFrame(list(mylist[0][0]), columns=["hex_id"])
 
-        if not res.empty:
+            if not res.empty:
 
-            test = hexagons_dataframe_to_geojson(res, file_output=None, column_name="hex_id")
-            m = folium.Map(location=[40.70, -73.94], zoom_start=2, tiles="CartoDB positron")
-            geo_j = folium.GeoJson(data=test, style_function=lambda x: {"fillColor": "orange"})
-            geo_j.add_to(m)
+                test = hexagons_dataframe_to_geojson(res, file_output=None, column_name="hex_id")
+                p = folium.Map(location=[40.70, -73.94], zoom_start=2, tiles="CartoDB positron")
+                geo_j = folium.GeoJson(data=test, style_function=lambda x: {"fillColor": "orange"})
+                geo_j.add_to(p)
 
-            st_data = folium_static(m)
-            
-            st.write('Shortest path (H3 list)')
-            st.write(mylist[0][0])
-            st.write(st_data)
+                st_data = folium_static(p)
+                
+                st.write('Shortest path (H3 list)')
+                st.write(mylist[0][0])
+                st.write(st_data)
 
-    except:
-        pass
+        except:
+            pass
 
 driver.close()
